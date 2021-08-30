d07f36d4ab7abb377b0007dff8b8f07ac49f6fde
Michael Bideau <mica.devel@gmail.com>
2021-08-30 20:55:34 +0200

    fix: testing

--- test.sh
+++ test.sh
@@ -1,17 +1,46 @@
 #!/bin/bash
-#set -x
-#set -e
-test_dir=/disks/ssdbtrfs/bucko/test
-script=/home/bucko/btrfs-receive-go/btrfs-receive-go
-mkdir -p "$test_dir"
-cd "$test_dir"
-[ -d rw ] && btrfs subvolume delete rw > /dev/null
-[ -n "$(ls snaps)" ] && for X in snaps/*; do
-	btrfs subvolume delete "$X" > /dev/null
-done
-btrfs subvolume create rw > /dev/null
-mkdir -p snaps
-btrfs subvolume snapshot -r rw snaps/000 > /dev/null
+
+set -e
+
+THIS_DIR="$(realpath "$(dirname "$0")")"
+
+BTRFS_DIFF_BIN="$THIS_DIR"/btrfs-diff
+TEST_DIR="$THIS_DIR"/testdir
+DATA_DIR="$TEST_DIR"/data
+SNAPS_DIR="$TEST_DIR"/snaps
+
+if [ ! -x "$BTRFS_DIFF_BIN" ]; then
+    echo "Binary '' not found or not executable" >&2
+    exit 1
+fi
+
+echo "Test directory: '$TEST_DIR'"
+
+[ ! -d "$TEST_DIR" ] && mkdir -p "$TEST_DIR"
+
+use_sudo=
+if [ "$(id -u)" != '0' ]; then
+    use_sudo=sudo
+fi
+
+echo "-- Removing existing data and snapshots"
+[ -d "$DATA_DIR" ] && $use_sudo btrfs subvolume delete "$DATA_DIR" > /dev/null
+if [ -d "$SNAPS_DIR" ]; then
+    [ -n "$(ls "$SNAPS_DIR")" ] && for snap in "$SNAPS_DIR"/*; do
+        $use_sudo btrfs subvolume delete "$snap" > /dev/null
+    done
+fi
+
+echo "-- Creating a subvolume that will contains the data: '$DATA_DIR' (read-write)"
+btrfs subvolume create "$DATA_DIR" > /dev/null
+
+echo "-- Creating a subvolume that will contains the snapshots: '$SNAPS_DIR' (read-only)"
+[ ! -d "$SNAPS_DIR" ] && mkdir -p "$SNAPS_DIR"
+
+echo "-- Creating first snapshot (with no data)"
+btrfs subvolume snapshot -r "$DATA_DIR" "$SNAPS_DIR"/000 > /dev/null
+
+echo "-- Creating data and snapshots for each commands"
 I=1
 (
 cat <<END
@@ -23,22 +52,48 @@ ln bar/baz_file bar/baaz_file
 mv bar/baz_file bar/foo_file
 rm bar/foo_file
 rm -rf bar
+mkdir dir
+touch dir/file
+mkfifo dir/fifo
+ln dir/file dir/hardlink
+ln -s file dir/symlink
+mv dir/hardlink dir/hardlink.rn
+mv dir/symlink dir/symlink.rn
+mv dir/fifo dir/fifo.rn
+echo todel > dir/file_to_del
+rm -rf dir
 END
-) | while read command; do
-	(cd rw; sh -c "$command")
-	btrfs subvolume snapshot -r rw snaps/$(printf "%03i" $I) > /dev/null
-	I=$(($I + 1))
+) | while read -r command; do
+    (cd "$DATA_DIR"; sh -c "$command")
+    btrfs subvolume snapshot -r "$DATA_DIR" "$SNAPS_DIR/$(printf "%03i" $I)" > /dev/null
+    echo "$I: $command" >&2
+    I=$((I + 1))
 done
-for A in snaps/*; do
-	for B in snaps/*; do
-		if [ "$A" = "$B" ]; then continue; fi
-		"$script" $test_dir/"$A" $test_dir/"$B" > /tmp/a_raw 2>&1
-		cat /tmp/a_raw | grep -A 100 GENERATED|grep -v GENERATED|cut -b10-|sort | grep -v '^changed: $' > /tmp/a || true
-		diff -qr "$A" "$B" | \
-			sed "s|$A|old|; s|$B|new|g; s|: |/|; s/Only in new/  added: /; s/Only in old/deleted: /; s|Files old/.* and new/\(.*\) differ|changed: /\1|" | \
-			sort > /tmp/b || true
-		# Filter things that were spuriously added (can happen due to utimes changes and stuff).
-		# Then filter only changes (else we spit out headers for the stuff we filtered).
-		diff -u5 /tmp/a /tmp/b | grep -v '^-changed' | grep '^[+-][^+-]' | sed "s|^|$A $B: |"
-	done
+
+echo "-- Comparing snapshots between them with '$(basename "$BTRFS_DIFF_BIN")' "`
+     `"then with 'diff' and printing unmatching lines (between both diff)"
+failed=false
+for A in "$SNAPS_DIR"/*; do
+    for B in "$SNAPS_DIR"/*; do
+        if [ "$A" = "$B" ]; then continue; fi
+        $use_sudo "$BTRFS_DIFF_BIN" "$A" "$B" > /tmp/a_raw 2>&1 || true
+        cut -b4- /tmp/a_raw | sort | grep -v '^changed: $' > /tmp/a || true
+        LC_ALL=C diff -qr "$A" "$B" | \
+            sed "s|$A|old|; s|$B|new|g; s|: |/|; s/Only in new/  added: /; s/Only in old/deleted: /; s|Files old/.* and new/\(.*\) differ|changed: /\1|" | \
+            sed "/File .* is a fifo while file .* is a fifo/d" | \
+            sort > /tmp/b || true
+        # Filter things that were spuriously added (can happen due to utimes changes and stuff).
+        # Then filter only changes (else we spit out headers for the stuff we filtered).
+        if ! LC_ALL=C diff -u5 /tmp/a /tmp/b >/tmp/b_diff && \
+                [ "$(cat -s /tmp/b_diff | grep -v '^-changed' | grep -c '^[+-][^+-]')" -ne 0 ]; then
+            echo "FAIL: $A $B" | sed "s|$TEST_DIR/\?||g"
+            cat -s /tmp/b_diff | grep -v '^-changed' | grep '^[+-][^+-]' | sed "s|^|$A $B: |" | sed "s|$TEST_DIR/\?||g"
+            failed=true
+        fi
+    done
 done
+if [ "$failed" = 'false' ]; then
+    echo "SUCCESS"
+else
+    exit 1
+fi
