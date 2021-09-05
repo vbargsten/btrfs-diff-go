vbargsten/btrfs-diff-go

commit 0c809e667ce85cf2c6e82537e50c197406b41c7f

feat: output times modifications as 'times' not as 'changed'

pkg/btrfs-diff.go
@@ -41,12 +41,13 @@ const (
 	opIgnore
 	opCreate
 	opModify
+	opTimes
 	opDelete
 	opRename // Special cased -- we need two paths
 	opEnd
 )
 
-var names []string = []string{"!!!", "ignored", "added", "changed", "deleted", "renamed", "END"}
+var names []string = []string{"!!!", "ignored", "added", "changed", "times", "deleted", "renamed", "END"}
 
 func (op operation) String() string {
 	return names[op]
@@ -400,6 +401,12 @@ func (diff *diffInst) Changes() []string {
 				if name == "/" && newFiles[name].Name == "" {
 					if debug {
 						fmt.Fprintf(os.Stderr, "[DEBUG] not appending %v (node.Children: nil, node.ChangeType:%v, new_node:%v)\n", name, opUnspec, newFiles[name])
+					}
+				// time modification
+				} else if newFiles[name].ChangeType == opTimes {
+					ret = append(ret, fmt.Sprintf("%10v: %v", opTimes, name))
+					if debug {
+						fmt.Fprintf(os.Stderr, "[DEBUG] appended (node.Children == nil): %10v: %v (%v) (%v)\n", opTimes, name, newFiles[name], node)
 					}
 				} else {
 					// TODO diff equality only
@@ -821,7 +828,7 @@ func SetDebug(status bool) {
 // SetUtimeOpModify consider the Utime instruction as a modification
 func SetUtimeOpModify() {
 	if debug {
-		fmt.Fprintf(os.Stderr, "[DEBUG] Utimes will count as modification\n")
+		fmt.Fprintf(os.Stderr, "[DEBUG] Utimes will not be ignored\n")
 	}
-	commandsDefs[C.BTRFS_SEND_C_UTIMES] = commandType{Name: "BTRFS_SEND_C_UTIMES", Op: opModify}
+	commandsDefs[C.BTRFS_SEND_C_UTIMES] = commandType{Name: "BTRFS_SEND_C_UTIMES", Op: opTimes}
 }

commit 89b1229802679d02a7e2ebbed1359040cb7b6d8b

fix: too many nodes detected as added (bug introduced by commit 0c809e6)

pkg/btrfs-diff.go
@@ -166,11 +166,10 @@ func (diff *diffInst) tagPath(path string, changeType operation) {
 	// not a deletion
 	} else {
 
-		// TODO replace those two op by just different from the opUnSpec
-		// Why this? if fileNode.Original != nil {
-		if !(fileNode.ChangeType == opCreate && changeType == opModify) {
+		// not a creation nor the current operation a change
+		if (fileNode.ChangeType != opCreate || (changeType != opModify && changeType != opTimes)) {
 			if debug {
-				fmt.Fprintf(os.Stderr, "[DEBUG]            node ChangeType is different from '%v' and '%v' (%v)\n", opCreate, opModify, fileNode.ChangeType)
+				fmt.Fprintf(os.Stderr, "[DEBUG]            current operation is not a modification, or the current node ChangeType is not '%v' (%v)\n", opCreate, fileNode.ChangeType)
 				fmt.Fprintf(os.Stderr, "[DEBUG]            replacing it with current operation '%v'\n", changeType)
 			}
 			fileNode.ChangeType = changeType